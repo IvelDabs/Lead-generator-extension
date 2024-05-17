e7349e6570916c1d8ecf4b904f82378c35a4a128
Daberechi Levi Nwachukwu <daberelevi@gmail.com>
2024-05-17 02:13:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let myLeads = []
-
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
-const deleteBtn = document.getElementById('delete-btn') 
-const deleteBtn2 = document.getElementById('delete-btn2') 
+const ulEl2 = document.getElementById('ul-el-2')
+const deleteAllBtn = document.getElementById('delete-btn') 
+const deleteFistEntryBtn = document.getElementById('delete-btn2') 
+const followUpBtn = document.getElementById('to-follow-up')
+const tabBtn = document.getElementById('tab-btn')
 
 // Code To Ensure that leads are continually stored in local storage and displayed even a session refresh
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById('tab-btn')
 
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    render(myLeads)
+    renderToGenerated(myLeads)
 }
 
 tabBtn.addEventListener('click', () => {
@@ -26,12 +27,12 @@ tabBtn.addEventListener('click', () => {
             myLeads.push(tabs[0].url);
             inputEl.value = "";
             localStorage.setItem("myLeads", JSON.stringify(myLeads));
-            render(myLeads);
+            renderToGenerated(myLeads);
         }
     );
 });
 
-function render(leads){
+function renderToGenerated(leads){
     let listItems = ''
     for(i=0; i < leads.length; i++){
     listItems += `  
@@ -42,15 +43,38 @@ function render(leads){
     ulEl.innerHTML = listItems
 }
 
-deleteBtn.addEventListener('dblclick', () => {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
-
 inputBtn.addEventListener('click', () => {
     myLeads.push(inputEl.value)
     inputEl.value = ''
     localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    render(myLeads)
+    renderToGenerated(myLeads)
+})
+
+deleteAllBtn.addEventListener('dblclick', () => {
+    localStorage.clear()
+    myLeads = []
+    renderToGenerated(myLeads)
+    renderToFocus(myLeads)
+})
+
+deleteFistEntryBtn.addEventListener('dblclick', () => {
+    myLeads.shift()
+    renderToGenerated(myLeads)
+})
+
+function renderToFocus(leads){
+    let listItems = ''
+    for(i=0; i < leads.length; i++){
+    listItems += `  
+        <li>
+            <a href = '${leads[i] }'  target = '_blank'> ${leads[i]} </a>
+        </li>`
+    }
+    ulEl2.innerHTML = listItems
+}
+
+followUpBtn.addEventListener('click', () => {
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    renderToFocus(myLeads)
+    ulEl.innerHTML = ' '
 })
